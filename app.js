a68cd1a34bfff14fc232a98b7205a965d02c9c48
Deola Richard <adekunlerichie5@gmail.com>
2024-02-13 17:54:55 +0000

    Full Pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, it is time to learn aws!'));
+app.get('/', (req, res) => res.send('Hello, it is time to learn aws! we are on pipeline testing'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
